9cafc22df8701b1031d965d425a2b34e6d000e68
Cole Horner <colehorner94@gmail.com>
2018-10-03 14:00:41 -0500

    WORKING hyperlinks!
    Finally fixed hyperlinks to work

--- Newspaper/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/About_Us.java
+++ Newspaper/TheCornellianApp/app/src/main/java/t/angelicafabila/thecornellianapp/About_Us.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class About_Us extends AppCompatActivity {
 
@@ -18,6 +20,31 @@ public class About_Us extends AppCompatActivity {
         configureAboutUsButton();
         configureContactUsButton();
         configureArchiveButton();
+
+        /*Editor chief's hyperlink to their bio. */
+        TextView editorChiefHyperlink = (TextView) findViewById(R.id.EditorChief);
+        editorChiefHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*ANGELS' hyperlink to their bio. */
+        TextView assistantEditorChiefHyperlink = (TextView) findViewById(R.id.AssistantEditorChief);
+        assistantEditorChiefHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*News editors's hyperlink to their bio. */
+        TextView newsEditorHyperlink = (TextView) findViewById(R.id.NewsEditor);
+        newsEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*Feature Editors's hyperlink to their bio. */
+        TextView featuresEditorHyperlink = (TextView) findViewById(R.id.FeaturesEditor);
+        featuresEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*Art and Entertainment Editors's hyperlink to their bio. */
+        TextView artEntertainmentEditorHyperlink = (TextView) findViewById(R.id.ArtEntertainmentEditor);
+        artEntertainmentEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*Opinion Editor's hyperlink to their bio. */
+        TextView opinionEditorHyperlink = (TextView) findViewById(R.id.OpinionEditor);
+        opinionEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*Copy editor's hyperlink to their bio. */
+        TextView copyEditorHyperlink = (TextView) findViewById(R.id.CopyEditor);
+        copyEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
+        /*Layout editor's hyperlink to their bio. */
+        TextView layoutEditorHyperlink = (TextView) findViewById(R.id.LayoutEditor);
+        layoutEditorHyperlink.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     private void configureMainButton()
